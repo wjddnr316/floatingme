66a88c9184743d65013cb050a6c14320b786366f
jeonguk seo <wjddnr316@naver.com>
2019-08-14 22:16:20 +0900

    js scroll수정

--- js/portfolio_custom.js
+++ js/portfolio_custom.js
@@ -24,12 +24,12 @@
                 },500);
                 return false;
             });
-     
+
             $("li.gnb-menu a").click(function(){      
                 $('html,body').animate({scrollTop:$(this.hash).offset().top}, 400);
                 return false;
             });
-     
+
             //what i do 댑스 에니메이션 컨트롤
             $("p.stop").click(function(){
                $(".skill-title-wrap h1").addClass("off");
@@ -39,67 +39,83 @@
                $(".skill-title-wrap h1").removeClass("off");
                 return false;
             });
-            
+
             //프로젝트메뉴
             $("li.project-menu").click(function(){
-                $(this).parents(".project").find(".introduce-wrap").removeClass("active");
-                $(this).parents('article.project').addClass("active");
+                var clickTg = $(this).parents(".project");
+                clickTg.find(".introduce-wrap").removeClass("active");
+                clickTg.addClass("active");
                 $("div.introduce-wrap",this).addClass("active");
-            });
-            $("div.close-btn").click(function(){
-               $(this).parents(".project").removeClass("active").find(".introduce-wrap").removeClass("active"); 
+                $(window).on("scroll",function(){//스크롤
+                    var scr = $(window).scrollTop();
+                    var scrBottom = $(window).scrollTop() + $(window).height();
+                    var width = $(window).width();
+                    var el = clickTg.position().top;
+                    var nextEl = el + clickTg.height();
+                    /*if(width > 1024){//데스크탑*/
+                    if ( scr >= nextEl || scrBottom <= el){
+                        var menu = clickTg.find(".introduce-wrap");
+                        $(clickTg,menu).removeClass("active");
+                        $(".introduce-wrap").removeClass("active");
+                        /*};*/
+                        
+                        /*if (scr >= 2800 || scr <= 1200){
+                            $("article.yuhan-project, article.yuhan-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if (scr >= 3600 || scr <= 2200){
+                            $("article.jansport-project, article.jansport-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if (scr >= 4500 || scr <= 3040){
+                            $("article.ju-project, article.ju-project li.project-menu div.introduce-wrap").removeClass("active");    
+                        }
+                        if (scr >= 5450 || scr <= 3900){
+                            $("article.aladin-project, article.aladin-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if (scr >= 6300 || scr <= 4800){
+                            $("article.disney-project, article.disney-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }*/
+                    }
+                    /*if (width <= 1023){//테블릿
+                        if ( scr >= 3500 || scr <= 2100){
+                            $("article.yuhan-project, article.yuhan-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if ( scr >= 4100 || scr <= 2850){
+                            $("article.jansport-project, article.jansport-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if ( scr >= 4900 || scr <= 3550){
+                            $("article.ju-project, article.ju-project li.project-menu div.introduce-wrap").removeClass("active");    
+                        }
+                        if ( scr >= 5600 || scr <= 4300){
+                            $("article.aladin-project, article.aladin-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                        if ( scr >= 6400 || scr <= 5000){
+                            $("article.disney-project, article.disney-project li.project-menu div.introduce-wrap").removeClass("active");
+                        }
+                    }*/
+                });
+                $("div.close-btn").click(function(){
+                   $(this).parents(".project").removeClass("active").find(".introduce-wrap").removeClass("active"); 
+                });
             });
             $(".view_more").click(function(){
                $(".aladin-project,.disney-project").slideDown(500); 
                 $(this).parent(".view_more_btn").css("display","none");
                 return false;
             });
-        });
         
         $(window).on("scroll",function(){//스크롤
-            var scr=$(window).scrollTop();
-            var width=$(window).width();
-            if (scr >= 1000){//탑버튼
-                    $("aside").fadeIn();
-                    $(".today_weather").css("right","80px");
-                }else{
-                    $("aside").fadeOut();
-                    $(".today_weather").css("right","20px");
-            }
-            /*$("#scrolltop").text(scr) ;*/
-            
-            if(width > 1024){//데스크탑
-                if (scr >= 2800 || scr <= 1200){
-                    $("article.yuhan-project, article.yuhan-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if (scr >= 3600 || scr <= 2200){
-                    $("article.jansport-project, article.jansport-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if (scr >= 4500 || scr <= 3040){
-                    $("article.ju-project, article.ju-project li.project-menu div.introduce-wrap").removeClass("active");    
-                }
-                if (scr >= 5450 || scr <= 3900){
-                    $("article.aladin-project, article.aladin-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if (scr >= 6300 || scr <= 4800){
-                    $("article.disney-project, article.disney-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-            }
-            if (width <= 1023){//테블릿
-                if ( scr >= 3500 || scr <= 2100){
-                    $("article.yuhan-project, article.yuhan-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if ( scr >= 4100 || scr <= 2850){
-                    $("article.jansport-project, article.jansport-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if ( scr >= 4900 || scr <= 3550){
-                    $("article.ju-project, article.ju-project li.project-menu div.introduce-wrap").removeClass("active");    
-                }
-                if ( scr >= 5600 || scr <= 4300){
-                    $("article.aladin-project, article.aladin-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-                if ( scr >= 6400 || scr <= 5000){
-                    $("article.disney-project, article.disney-project li.project-menu div.introduce-wrap").removeClass("active");
-                }
-            }
-        });
+                var scr=$(window).scrollTop();
+                if (scr >= 1000){//탑버튼
+                        $("aside").fadeIn();
+                        $(".today_weather").css("right","80px");
+                    }else{
+                        $("aside").fadeOut();
+                        $(".today_weather").css("right","20px");
+                };
+                /*$("#scrolltop").text(scr) ;
+                var scrollBottom = $(window).scrollTop() + $(window).height();
+                $("#scrollbottom").text(scrollBottom);*/
+          });
+});
+        
+        
